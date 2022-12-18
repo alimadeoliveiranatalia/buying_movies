3be8e8c0c5824b762d5452acdb6a4019bd7524fe
Natalia <alimadeoliveiranatalia@gmail.com>
2022-12-18 16:12:52 -0300

    feat: add args in Button components

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,13 +1,21 @@
 import { ComponentMeta } from "@storybook/react";
 import { Button, ButtonProps } from "./Button"
-import { Icon } from "./Icon"
 
 export default {
     title: 'components/Button',
-    component: Button,
-    subcomponents: [Icon]
-} ;
+    component: Button
+} as ComponentMeta<typeof Button>
 
-export function Default(args: ButtonProps){
-    return <Button {...args} />
+export function Default(){
+    return <Button title="" iconName="action" selected={true} />
 }
+
+export const Action = () => <Button title="action" iconName="action" selected={true}/>
+
+export const Comedy = () => <Button title="comedy" iconName="comedy" selected={true}/>
+
+export const Documentary = () => <Button title="documentary" iconName="documentary" selected={true}/>
+
+export const Horror = () => <Button title="horror" iconName="horror" selected={true}/>
+
+export const Family = () => <Button title="family" iconName="family" selected={true}/>
